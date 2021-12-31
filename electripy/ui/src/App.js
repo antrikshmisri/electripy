e9cd5ac4956649258952db2ace0b4843e5215a0e
antrikshmisri <antrikshmisri@gmail.com>
2021-12-31 21:28:49 +0530

    feat: temp button element (TBR later)

--- electripy/ui/src/App.js
+++ electripy/ui/src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./App.css";
+import Button from "./components/Button";
+import Paragraph from "./components/Paragraph";
+import Image from "./components/Image";
 
 import { eel } from "./eel.js";
 
 const App = () => {
   eel.set_host("http://localhost:8888");
-  return <></>;
+  return (
+    <Button
+        id="1234"
+        className="btn"
+        onClick={() => {
+          console.log("Clicked");
+        }}
+        style={
+          {backgroundColor: "#f2321d", width: "100px", height: "50px"}
+        }
+      >
+        <Paragraph id="2312" className="btn-text" text="Electripy⚡" />
+        <Image alt="python icon" src="https://img.icons8.com/ios-glyphs/30/000000/python.png" size={[200, 200]} id="2131" className="btn-logo"/>
+    </Button>
+  );
 };
 
 export default App;
